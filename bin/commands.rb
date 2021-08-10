9db1fafe04ba51a016e86bb5dce61a1f8c9725c9
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-08-10 15:09:12 -0400

    progress and mmore testing

--- bin/commands.rb
+++ bin/commands.rb
@@ -2,6 +2,8 @@
 
 first_arg, second_arg, *the_rest = ARGV
 
-puts "first arg - #{first_arg}"
-puts "second arg - #{second_arg}"
-puts "other args - #{the_rest}"
+puts "command - #{first_arg}"
+puts "generator - #{second_arg}"
+puts "options - #{the_rest}"
+
+
